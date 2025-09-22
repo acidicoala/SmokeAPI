f5b8c0c89f669afe44ed3e78de21a4030c830ea8
acidicoala <67734819+acidicoala@users.noreply.github.com>
2025-09-22 22:47:53 +0500

    Added support for self-injection via `winhttp.dll`

--- src/main_win.cpp
+++ src/main_win.cpp
@@ -1,11 +1,11 @@
+// ReSharper disable CppUnusedIncludeDirective
 #include <koalabox/win.hpp>
 
 #include "smoke_api/smoke_api.hpp"
 
 // These headers will be populated at build time
 #include "linker_exports_for_steam_api.h"
-#include "linker_exports_for_version.h"
-#include "linker_exports_for_winmm.h"
+#include "linker_exports_for_windows_dlls.h"
 
 DLL_MAIN(void* handle, const uint32_t reason, void*) {
     if(reason == DLL_PROCESS_ATTACH) {
